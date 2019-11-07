0cfe45c0ff4b32c24b62388167411e5d140200b7
root <k9779k@naver.com>
2019-11-07 15:04:04 +0900

    nodejsServerPush

---
+++ .vscode-server/bin/6ab598523be7a800d7f3eb4d92d7ab9a66069390/out/vs/server/uriTransformer.js
@@ -0,0 +1,5 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------------------------------------------*/
+module.exports=function(e){return{transformIncoming:e=>"vscode-remote"===e.scheme?{scheme:"file",path:e.path}:"file"===e.scheme?{scheme:"vscode-local",path:e.path}:e,transformOutgoing:o=>"file"===o.scheme?{scheme:"vscode-remote",authority:e,path:o.path}:"vscode-local"===o.scheme?{scheme:"file",path:o.path}:o,transformOutgoingScheme:e=>"file"===e?"vscode-remote":"vscode-local"===e?"file":e}};
+//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/6ab598523be7a800d7f3eb4d92d7ab9a66069390/core/vs/server/uriTransformer.js.map
