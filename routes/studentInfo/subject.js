6d188fd978c809f8243fde1fc791d08bd83b5ed9
hain9563 <32196586+hain9563@users.noreply.github.com>
2019-11-18 13:08:55 +0900

    학과 클릭시 해당학과의 전공과목 리스트 가져오기 추가

--- routes/studentInfo/subject.js
+++ routes/studentInfo/subject.js
@@ -1,12 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-var mysqlDB = require('../mysql-db');
+var mysqlDB = require('../../mysql-db');
 
-// /studentInfo/majorlist
+//router.set('view engine', 'ejs');
+//router.set('views', __dirname +'/views');
+
+
+// /subject
+
+/* GET studentInfo listing. /subject
+router.get('/', function(req, res, next) {
+    res.render(subject.ejs);
+  });
+*/
+
+// /studentInfo/subject/majorlist
 /* 전공 클릭시 학과 리스트 가져오기 */
 router.get('/majorlist', function(req, res, next) {
-    var sql = 'select * from majorsubject;';
+    var sql = 'select major from Graduation_requirement;';
 
     mysqlDB.query(sql, [], function(error, major) {
         if(error == null) {
@@ -24,13 +36,13 @@ router.get('/majorlist', function(req, res, next) {
             });
         }
     });
-    
+
 });
 
-// /studentInfo/nonmajorlist
+// /studentInfo/subject/nonmajorlist
 /* 교양 클릭시 교양과목 리스트 가져오기 */
-router.get('/majorlist', function(req, res, next) {
-    var sql = 'select * from nonmajorsubject;';
+router.get('/nonmajorlist', function(req, res, next) {
+    var sql = 'select subject_name from nonmajorsubject;';
 
     mysqlDB.query(sql, [], function(error, nonmajor) {
         if(error == null) {
@@ -48,9 +60,37 @@ router.get('/majorlist', function(req, res, next) {
             });
         }
     });
-    
+
 });
 
+// /studentInfo/subject/majorlist/:majorParam
+/* 학과 클릭시 해당학과의 전공과목 리스트 가져오기 */
+router.get('/majorlist/:majorsubject', function(req, res, next) {
+    //res.send(req.params);
+
+    var major = req.params.majorsubject;
+    //console.log(major);
+    res.send(major);
+
+    var sql = 'select subject_name from majorsubject where major = ?'
+    mysqlDB.query(sql, major,function(err, result){
+        if(error == null) {
+            console.log(major);
+            res.json({
+                "code" : 200,
+                "result" : result
+            });
+        }
+        else{
+            console.log(error);
+            res.json({
+                "code" : 400,
+                "result" : "failed"
+            });
+        }
+    });
+
+});
 
 
 
--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const http = require('http')
 const app = require('./app')
 
 http.createServer(app).listen(8888,() => {
-    console.info(`Backend Server is running on 8888..`)
+    console.log(`Backend Server is running on 8888..`)
 })
