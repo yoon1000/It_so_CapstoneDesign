2f05a4a7c8c9a15ffcfbccb0f981cec2a2dc3753
YUN9701 <k9779k@naver.com>
2019-11-24 07:46:31 +0900

    port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http')
 const app = require('./app')
 
-http.createServer(app).listen(1111,() => {
-    console.log(`Backend Server is running on 1111..`)
+http.createServer(app).listen(5555,() => {
+    console.log(`Backend Server is running on 5555..`)
 })
