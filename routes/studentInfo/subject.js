dea7c3f658b36badda0c6f8ec25e6f8aaeebe0fd
hain9563 <32196586+hain9563@users.noreply.github.com>
2019-11-24 11:25:51 +0900

    POST /majorlist POST /nonmajorlist api 수정

--- routes/studentInfo/subject.js
+++ routes/studentInfo/subject.js
@@ -129,6 +129,7 @@ router.post('/majorlist', function (req, res){
         //console.log(split[i]);
     }*/
     console.log(req.body.subject);
+    console.log(req.body.subject.toString());
     var string = req.body.subject.replace('\"', '');
     console.log(string);
     var string2 = string.replace('\\[','');
