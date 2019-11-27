df24f352c370721a033c08131e9ea91fac93d6ff
YUN9701 <k9779k@naver.com>
2019-11-27 21:36:38 +0900

    학기별

--- routes/studentInfo/main.js
+++ routes/studentInfo/main.js
@@ -97,9 +97,8 @@ router.post('/nonmajorCredit', function(req, res, next) {
 // /studentInfo/main/semester
 /* 전공그래프클릭시 학기별 수강하지 않은 전공과목 가져오기 */
 router.post('/semester', function(req, res, next) {
-    var semester = parseInt(req.body.semester);
     var sql = 'select subject_name from majorsubject where subject_name NOT IN (select subject_name from Student_majorsubject where id = ?) AND major = ? AND semester = ?';
-    mysqlDB.query(sql, [req.body.id, req.body.major, semester], function(error, result) {
+    mysqlDB.query(sql, [req.body.id, req.body.major, req.body.semester], function(error, result) {
         if(error == null) {
             res.json({
                 "code" : 200,
