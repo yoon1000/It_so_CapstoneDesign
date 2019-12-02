88efc0ac71a432d474626d6afc9571265031120a
hain9563 <32196586+hain9563@users.noreply.github.com>
2019-12-02 19:37:44 +0900

    /nonmajorlist -> /StudnetNonmajorlist api 이름 수정

--- routes/studentInfo/subject.js
+++ routes/studentInfo/subject.js
@@ -179,7 +179,7 @@ router.post('/majorlist', function (req, res){
 
 /*/studentInfo/subject/nonmajorlist
 클라이언트로 부터 받은 해당 학생의 학적정보 db에 반영하기*/
-router.post('/nonmajorlist', function (req, res){
+router.post('/StudentNonmajorlist', function (req, res){
     var id = req.body.id;
     var nonmajor_list = req.body.subject2;//학생이 들은 과목들
     //var length = Object.keys(nonmajor_list).length;//과목의 개수
@@ -197,10 +197,11 @@ router.post('/nonmajorlist', function (req, res){
     +' on s.school = n.school'
     +' where s.id = ';
 
+
     for(var i=0; i<split.length;i++) {
         query += sql + '\'' + id + '\'' + ' AND n.subject_name = ' + '\'' + split[i] + '\'' + ';'
     }
- 
+    console.log(query);
     mysqlDB.query(sql, [], function(error, result) {
         if(error == null) {
             res.json({
