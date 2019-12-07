cc4896fe67e81f84842cbbb4cae37205ecb54157
YUN9701 <k9779k@naver.com>
2019-12-07 15:50:48 +0900

    DB변경

--- config/mysql-db.js
+++ config/mysql-db.js
@@ -1,8 +1,8 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-    host : '106.10.58.65',
-    port : 18081,
+    host : '106.10.50.18',
+    port : 18090,
     user : 'root',
     password : 'Test!@#123',
     database : 'muse',
